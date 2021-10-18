0bae390228719c6471ec8879348da9df968b3dde
WangManLex <alexandermaniwang99@gmail.com>
2021-10-18 08:26:11 -0400

    added event listener and empties player and computer hand when initiated.

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ let deckEl = document.getElementById('deck')
 
 
 /*-------------- Event Listeners --------------*/
-
+deckEl.addEventListener('click', handleClick)
 
 
 /*----------------- Functions -----------------*/
@@ -20,10 +20,13 @@ init()
 
 function init() {
   deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+  playerHand = []
+  computerHand = []
   isWinner = null
   render()
 }
 function render() {
+  //- Create render function displaying two drawn cards for each player (computer and player).
   
 }
 function handleClick() {
