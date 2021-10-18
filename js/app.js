edb55463067c980dab4a6b6bc5bb41565dae814f
WangManLex <alexandermaniwang99@gmail.com>
2021-10-18 15:13:24 -0400

    cardvalues made easier to understand

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 /*----------------- Constants -----------------*/
 let deck = []
 let playerHand = []
-let computerHand = []
-let isWinner, gameStatus, playerSum, computerSum
+let dealerHand = []
+let isWinner, gameStatus, playerSum, dealerSum, card, cardValue
 
 /*------------- Variables (state) -------------*/
 
@@ -20,66 +20,83 @@ init()
 
 function init() {
   deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+  // valueDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
   playerHand = []
-  computerHand = []
+  dealerHand = []
   playerSum = null
-  computerSum = null
+  dealerSum = null
   isWinner = null
   render()
 }
+
 function render() {
   //Create values for each of the card strings
-  cardValue(deck)
   
   //randomly select cards from deck to computer hand
-  starterCards()
+
   
   //- Create render function displaying two drawn cards for each player (computer and player).
-
+  
   //Create the game status message
-
+  
 }
+
+function cardValues(card) {
+  if (card === 'dK' || card === 'hK' || card === 'cK' || card === 'sK' ||
+  card === 'dQ' || card === 'hQ' || card === 'cQ' || card === 'sQ' ||
+  card === 'dJ' || card === 'hJ' || card === 'cJ' || card === 'sJ' ||
+  card === 'd10' || card === 'h10' || card === 'c10' || card === 's10'
+    ){cardValue = 10}
+    else if (card === 'd09' || card === 'h09' || card === 'c09' || card === 's09'){cardValue = 9}
+    else if (card === 'd08' || card === 'h08' || card === 'c08' || card === 's08'){cardValue = 8}
+    else if (card === 'd07' || card === 'h07' || card === 'c07' || card === 's07'){cardValue = 7}
+    else if (card === 'd06' || card === 'h06' || card === 'c06' || card === 's06'){cardValue = 6}
+    else if (card === 'd05' || card === 'h05' || card === 'c05' || card === 's05'){cardValue = 5}
+    else if (card === 'd04' || card === 'h04' || card === 'c04' || card === 's04'){cardValue = 4}
+    else if (card === 'd03' || card === 'h03' || card === 'c03' || card === 's03'){cardValue = 3}
+    else if (card === 'd02' || card === 'h02' || card === 'c02' || card === 's02'){cardValue = 2}
+  return cardValue
+}
+
 function handleClick() {
   if (playerHand.length < 2){
     let randIdx = Math.floor(Math.random() * deck.length)
     let cardPicked = deck.splice(randIdx, 1)
     playerHand.push(cardPicked)
-    render()
   }
+  render()
 }
+
 function starterCards() {
-  for (let i = 0; 2 > computerHand.length; i++) { 
+  for (let i = 0; 2 > dealerHand.length; i++) { 
     if (deck.length > 0){
       let randIdx = Math.floor(Math.random() * deck.length)
       let cardPicked = deck.splice(randIdx, 1)
-      computerHand.push(cardPicked)
+      dealerHand.push(cardPicked)
     }
     render()
   }
-    for (let i = 0; 2 > playerHand.length; i++) { 
-      if (deck.length > 0){
-        let randIdx = Math.floor(Math.random() * deck.length)
-        let cardPicked = deck.splice(randIdx, 1)
-        playerHand.push(cardPicked)
-      }
-      render()
+  for (let i = 0; 2 > playerHand.length; i++) { 
+    if (deck.length > 0){
+      let randIdx = Math.floor(Math.random() * deck.length)
+      let cardPicked = deck.splice(randIdx, 1)
+      playerHand.push(cardPicked)
     }
-}
-function cardValue(deck) {
-deck.splice(13,39)
-let valueDeck = deck
-let deckValues = valueDeck.map((card) => {
-  let splitCard = card.split('').slice(1, 3).join('')
-  return splitCard
-})
-console.log(deckValues)
-if (deckValues === '10', 'J', 'Q', 'K'){ return 10 }
-else {return `${deckValues}`}
+    render()
+  }
 }
 
 function cardCompare() {
-
+  
 }
+
+
+// valueDeck.splice(13,39)
+// let deckValues = valueDeck.map((card) => {
+//   let splitCard = card.split('').slice(1, 3).join('')
+//   return splitCard
+// })
+// console.log(deckValues)
 
 // Bust - basically losing (if hand is over 21)
 // Hit - drawing an extra card
